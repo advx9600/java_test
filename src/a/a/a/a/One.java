5b0863eb5427868433632b23e24ad685640689ce
advx9600 <nwz88348307@163.com>
2015-06-15 10:58:41 +0800

    source

---
+++ src/a/a/a/a/One.java
@@ -0,0 +1,21 @@
+package a.a.a.a;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.Map;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+
+import a.a.a.a.b.*;
+
+public class One {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HttpDownLoad.one();
+	}
+
+}
---
+++ src/template/one.ftl
@@ -0,0 +1,8 @@
+        public static String get${SEG}()
+        {
+            return getFiled(SEG_${SEG_STATIC});
+        }
+        public static void set${SEG}(String val)
+        {
+            setFiled(SEG_${SEG_STATIC}, val);
+        }
